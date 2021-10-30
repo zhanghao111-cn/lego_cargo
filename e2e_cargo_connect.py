ea422fd174159747ec9939f455f14446fb684cdd
Gao Leijia <leijia.gao@nokia.com>
2021-10-30 16:50:16 -0500

    init code

---
+++ e2e_cargo_connect.py
@@ -0,0 +1,72 @@
+#Import
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Object
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+hub = PrimeHub() 
+mm_motor = MotorPair("C","D")
+arm_motor = Motor("E")
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Function definition
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+def right_turn(degrees,speed,reset):
+    if(reset == 1):
+        mm_motor.set_default_speed(speed)
+    hub.motion_sensor.reset_yaw_angle()
+    while(hub.motion_sensor.get_yaw_angle()<degrees):
+        mm_motor.start(100)
+    mm_motor.stop()
+
+def left_turn(degrees,speed,reset):
+    if(reset == 1):
+        mm_motor.set_default_speed(speed)
+    hub.motion_sensor.reset_yaw_angle()
+    while((-hub.motion_sensor.get_yaw_angle())<degrees):
+        print(hub.motion_sensor.get_yaw_angle())
+        mm_motor.start(-100)
+    mm_motor.stop()
+
+def line_follow(speed,m_reflect):
+    print("line follow")
+    movement_motor.set_default_speed(speed)
+    while True:
+        movement_motor.start((m_reflect-col_sensor.get_reflected_light())*2)
+        if(col_sensor.get_color() == 'red'):
+            movement_motor.stop()
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Variable definition
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+switch_flag = 0
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Execution
+hub.light_matrix.show_image('HAPPY')
+while True:
+    hub.left_button.wait_until_pressed()
+    if(switch_flag == 1):
+        hub.light_matrix.write('3')
+        print("Round3")
+#Round 3 start
+        right_turn(90,20,1)
+        left_turn(90,20,1)
+#Round 3 end
+    if(switch_flag == 0):
+        hub.light_matrix.write('1')
+        print(switch_flag)
+        print("Round1")
+#Round 1 start
+        right_turn(90,20,1)
+#Round 1 end
+    switch_flag = 1
+    hub.right_button.wait_until_pressed()
+    hub.light_matrix.write('2')
+    print("Round2")
+#Round 2 start
+    left_turn(90,20,1)
+#Round 2 end
