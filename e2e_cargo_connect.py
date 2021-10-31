8da0e6292ebfb694ff419d0132f3469c7a53661c
kevinfhell <kevin.gao.love@gmail.com>
2021-10-31 14:25:09 -0500

    Update e2e_cargo_connect.py

--- e2e_cargo_connect.py
+++ e2e_cargo_connect.py
@@ -7,25 +7,26 @@ from math import *
 
 #Object
 #----------------------------------------------------------------------------------------------------------------------------------------------------
-hub = PrimeHub() 
+hub = PrimeHub()
 mm_motor = MotorPair("C","D")
 arm_motor = Motor("E")
+col_sensor = ColorSensor("B")
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Function definition
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 def right_turn(degrees,speed,reset):
+    mm_motor.set_default_speed(speed)
     if(reset == 1):
-        mm_motor.set_default_speed(speed)
-    hub.motion_sensor.reset_yaw_angle()
+        hub.motion_sensor.reset_yaw_angle()
     while(hub.motion_sensor.get_yaw_angle()<degrees):
         mm_motor.start(100)
     mm_motor.stop()
 
 def left_turn(degrees,speed,reset):
+    mm_motor.set_default_speed(speed)
     if(reset == 1):
-        mm_motor.set_default_speed(speed)
-    hub.motion_sensor.reset_yaw_angle()
+        hub.motion_sensor.reset_yaw_angle()
     while((-hub.motion_sensor.get_yaw_angle())<degrees):
         print(hub.motion_sensor.get_yaw_angle())
         mm_motor.start(-100)
@@ -33,11 +34,11 @@ def left_turn(degrees,speed,reset):
 
 def line_follow(speed,m_reflect):
     print("line follow")
-    movement_motor.set_default_speed(speed)
+    mm_motor.set_default_speed(speed)
     while True:
-        movement_motor.start((m_reflect-col_sensor.get_reflected_light())*2)
+        mm_motor.start((m_reflect-col_sensor.get_reflected_light())*2)
         if(col_sensor.get_color() == 'red'):
-            movement_motor.stop()
+            mm_motor.stop()
 
 def move_arm_up(degree,speed):
     print("Move arm up")
@@ -58,30 +59,44 @@ def move_arm_down_turbo():
 #Variable definition
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 switch_flag = 0
+normal_speed = 40
+slow_speed = 20
+middle_reflection = 80
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Execution
 hub.light_matrix.show_image('HAPPY')
+hub.motion_sensor.reset_yaw_angle()
+"""
 while True:
     hub.left_button.wait_until_pressed()
     if(switch_flag == 1):
         hub.light_matrix.write('3')
         print("Round3")
 #Round 3 start
-        right_turn(90,20,1)
-        left_turn(90,20,1)
+        right_turn(90,10,0)
+        left_turn(90,10,0)
 #Round 3 end
     if(switch_flag == 0):
         hub.light_matrix.write('1')
         print(switch_flag)
         print("Round1")
 #Round 1 start
-        right_turn(90,20,1)
+        right_turn(90,10,0)
 #Round 1 end
     switch_flag = 1
     hub.right_button.wait_until_pressed()
     hub.light_matrix.write('2')
     print("Round2")
 #Round 2 start
-    left_turn(90,20,1)
+    left_turn(90,10,0)
 #Round 2 end
+"""
+"""
+mm_motor.move(25,'cm',0,normal_speed)
+right_turn(45,slow_speed,0)
+mm_motor.move(18,'cm',0,normal_speed)
+left_turn(90,slow_speed,1)
+mm_motor.move(18,'cm',0,normal_speed)
+"""
+line_follow(slow_speed, middle_reflection)
