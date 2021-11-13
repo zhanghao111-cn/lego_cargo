bc6cb3dfdc055452d53f28d80137520b2b37ed89
Gao Leijia <leijia.gao@nokia.com>
2021-11-13 13:05:04 -0600

    Add more function

--- e2e_cargo_connect.py
+++ e2e_cargo_connect.py
@@ -9,8 +9,11 @@ from math import *
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 hub = PrimeHub()
 mm_motor = MotorPair("C","D")
+right_motor = Motor("D")
+left_motor = Motor("C")
 arm_motor = Motor("E")
 col_sensor = ColorSensor("B")
+dis_sensor = DistanceSensor("F")
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Function definition
@@ -22,6 +25,13 @@ def right_turn(degrees,speed,reset):
         hub.motion_sensor.reset_yaw_angle()
     while(hub.motion_sensor.get_yaw_angle()<degrees):
         mm_motor.start(100)
+    mm_motor.stop()
+
+def reset_turn():
+    mm_motor.set_default_speed(normal_speed)
+   # hub.motion_sensor.reset_yaw_angle()
+    while(hub.motion_sensor.get_yaw_angle()>0):
+        mm_motor.start(-100)
     mm_motor.stop()
 
 def right_turn_motor(degrees,speed):
@@ -44,7 +54,7 @@ def left_turn_motor(degrees,speed):
     #mm_motor.move(degrees,'degrees',-100)
     mm_motor.move_tank(degrees,'degrees',0,50)
     mm_motor.stop()
-    
+
 def line_follow(speed,m_reflect):
     print("line follow")
     mm_motor.set_default_speed(speed)
@@ -66,20 +76,65 @@ def move_arm_down_turbo():
     arm_motor.start_at_power(100)
     arm_motor.run_for_degrees(180,100)
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------
+def gyro_straight(target_yawn, distance, power):
+    hub.motion_sensor.reset_yaw_angle()
+    right_motor.run_to_position(0)
+    while(right_motor.get_degrees_counted < (distance / 17.5 * 360)):
+        #print(distance / 17.5 * 360)
+        print(right_motor.get_degrees_counted())
+        correction = target_yawn - hub.motion_sensor.get_yaw_angle()
+        #mm_motor.start_at_power((power + correction), (power - correction))
+        #mm_motor.start_at_power((power - correction), (power + correction))
+        #mm_motor.move_tank(10,"degrees",int(power + correction), int(power - correction ))
+    
+ #       print(power + correction)
 
+    mm_motor.stop()
+
+def pid_turn(target_angle, p_constant, min_power):
+    hub.motion_sensor.reset_yaw_angle()
+    mm_motor.set_stop_action('hold')
+    error = target_angle - hub.motion_sensor.get_yaw_angle()
+    max_power = 100
+    while(error != 0):
+        error = target_angle - hub.motion_sensor.get_yaw_angle()
+        control_output = error - p_constant
+        if(abs(control_output) > max_power):
+            control_output = max_power * target_angle / abs(target_angle)
+        if(abs(control_output) < min_power):
+            control_output = min_power * target_angle / abs(target_angle)
+        mm_motor.start_at_power(control_output, -control_output)
+    mm_motor.stop()
+
+def pid_line_follow(kp,ki,kd,distance):
+    last_err = 0
+    integral = 0
+    motor_power = 27
+    target_light = 80
+    right_motor.run_to_position(0)
+    while(right_motor.get_position() > distance / 17.5 * 360):
+        error = target_light - col_sensor.get_reflected_light()
+        integral = error + integral
+        derivative = error - last_err
+        correction = (error * kp) + (integral * ki) + (derivative * kd)
+        mm_motor.start_at_power((motor_power + correction), (motor_power - correction))
+        last_err = error
+    mm_motor.stop()
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
 #Variable definition
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 switch_flag = 0 # use as a local variable to switch between round1 and round3
-normal_speed = 40 # normal speed for routine. 
+normal_speed = 40 # normal speed for routine.
 slow_speed = 20 #slow speed to control the accuracy
-middle_reflection = 80 # used for the line follower or accurate positioning. 
+middle_reflection = 80 # used for the line follower or accurate positioning.
 #----------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Execution
 hub.light_matrix.show_image('HAPPY')
 hub.motion_sensor.reset_yaw_angle()
-
+"""
 while True:
     hub.left_button.wait_until_pressed()
     if(switch_flag == 1):
@@ -146,7 +201,7 @@ while True:
     print("Round2")
 #Round 2 start
     wait_for_seconds(1)
-    mm_motor.move(600,"degrees",0)#step1    
+    mm_motor.move(600,"degrees",0)#step1
     right_turn_motor(90,normal_speed)#step2
     mm_motor.move(600,"degrees",0)
     mm_motor.move(-100,"degrees",0)
@@ -154,3 +209,53 @@ while True:
     right_turn_motor(90,normal_speed)
     mm_motor.move(-800,"degrees",0)
 #Round 2 end
+
+hub.motion_sensor.reset_yaw_angle()
+right_turn(50,30,0)
+mm_motor.move(600,"degrees",0)
+mm_motor.move(-150,"degrees",0)
+right_turn(90,30,0)
+mm_motor.move(-200,"degrees",0)
+hub.motion_sensor.reset_yaw_angle()
+while True:
+    if(dis_sensor.get_distance_cm() > 30):
+        mm_motor.start(0,10)
+ #   if(dis_sensor.get_distance_cm() < 31):
+ #       mm_motor.start(0,-10)
+    if(dis_sensor.get_distance_cm() <= 30):
+        mm_motor.stop()
+        break
+wait_for_seconds(1)
+#right_turn_motor(180,30)
+right_turn(90,30,0)
+mm_motor.move(1000,"degrees",0)
+
+hub.motion_sensor.reset_yaw_angle()
+right_turn(30,20,0)
+print(hub.motion_sensor.get_yaw_angle())
+mm_motor.move(600,"degrees",0)
+mm_motor.move(-300,"degrees",0)
+reset_turn()
+print(hub.motion_sensor.get_yaw_angle())
+wait_for_seconds(1)
+right_turn(166,50,0)
+print(hub.motion_sensor.get_yaw_angle())
+mm_motor.move(1000,"degrees",0)
+"""
+gyro_straight(0,10,70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
